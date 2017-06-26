f82a67cff00bbcddfcb6459e776a3aa591d76bf8
monishknair <monishknair@outlook.com>
2017-06-26 19:06:16 +0530

    models creation

--- CRoom/app/models.py
+++ CRoom/app/models.py
@@ -3,3 +3,22 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class ChatRoom(models.Model):
+
+    name = models.CharField(max_length=100)
+    max = models.IntegerField()
+
+    def __unicode__(self):
+        return self.name
+
+class Archive(models.Model):
+
+    _name = models.CharField(max_length=100)
+    _size = models.CharField(max_length=100)
+    _type = models.CharField(max_length=100)
+    _date_added = models.DateField(auto_now_add=True)
+    _owner = models.CharField(max_length=100)
+    _status = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self._name
--- CRoom/app/views.py
+++ CRoom/app/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+ 
+from app.models import ChatRoom,Archives
+ 
+class Chatrooms():
+  def get():
+    return render(request, 'app/index.html', {})
+  def post():
+    return render(request, 'app', {})
+  def put():
+    return None
+  def delete():
+    return None
 
-# Create your views here.
+ 
+class Members():
+  def get():
+    return render(request, 'app/index.html', {})
+  def post():
+    return render(request, 'app', {})
+  def put():
+    return None
+
+class Messages():
+  def get():
+    return render(request, 'app/index.html', {})
+  def post():
+    return render(request, 'app', {})
+  def put():
+    return None
+  def delete():
+    return None
